12214470ba6735e6737cd8fe107e570132b839cc
sukhvindarsingh <sukhvindarsingh113@gmail.com>
2021-10-16 22:40:05 +0530

    changing uploading method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import Alert from "./components/Alert";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-// import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -39,7 +39,7 @@ function App() {
   };
   return (
     <>
-      {/* <Router> */}
+      <Router>
         <Navbar
           title="TextUtils"
           aboutText="About TextUtils"
@@ -49,20 +49,20 @@ function App() {
 
         <Alert alert={alert} />
         <div className="container my-3">
-          {/* <Switch>
+          <Switch>
             <Route exact path="/About">
               <About />
-            </Route> */}
-            {/* <Route exact path="/"> */}
+            </Route>
+            <Route exact path="/">
               <TextForm
                 showAlert={showAlert}
                 heading="Enter the text to analyze below"
                 mode={mode}
               />
-            {/* </Route> */}
-          {/* </Switch> */}
+            </Route>
+          </Switch>
         </div>
-      {/* </Router> */}
+      </Router>
     </>
   );
 }
