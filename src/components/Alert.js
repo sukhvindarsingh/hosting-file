d7e008c3b907ad4712b9b658f07c0483dd88b9e4
Sukhvindarsingh Gedu <55839333+sukhvindarsingh@users.noreply.github.com>
2024-07-01 17:21:19 +0530

    Update Alert.js

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -8,14 +8,12 @@ function Alert(props) {
   return (
     <div style={{height: '50px'}}>
       {props.alert && (
-        <div>
           <div
             className={`alert alert-${props.alert.type} alert-dismissible fade show`}
             role="alert"
           >
             <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
           </div>
-        </div>
       )}
     </div>
   );
